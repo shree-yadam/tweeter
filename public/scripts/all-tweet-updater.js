84fa42e14b5d81bbd8b43dcdb3f094fab32a6138
Shree Devi Yadam <shree.yadam@gmail.com>
2021-06-15 21:40:29 +0000

    added timeago library and checked display using single sample tweet

--- public/scripts/all-tweet-updater.js
+++ public/scripts/all-tweet-updater.js
@@ -1,4 +1,23 @@
+const tweet = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1623610194129
+};
+
 $(document).ready(function() {
+  //Change this after posting message to server
+  $('div.user-profile img').attr('src', tweet.user.avatars);
+  $('div.user-profile p').text(tweet.user.name);
+  $('.user-handle p').text(tweet.user.handle);
+  $('tweet').text(tweet.content.text);
+  $('.tweet-time').text(timeago.format(tweet.created_at));
+  //change code above
   $('.tweet').on('mouseover', function(event) {
     $(this).css('box-shadow', '5px 5px #778ba5')
   });
